f87fce6086e6d052826c5d2b036a9c276fd61663
mahojv <moisesmjavier@outlook.com>
2025-05-05 10:41:15 -0600

    feat: add GitHub icon SVG and update DashboardComponent layout

--- src/components/DashboardComponent.jsx
+++ src/components/DashboardComponent.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function DashboardComponent() {
     return (
-        <div className='bg-gray-400 h-screen w-full flex flex-col items-center py-10 gap-2'>
-            <div className='border w-[80%] h-80 rounded-3xl flex flex-col items-center gap-10 py-5 px-10 '>
+        <div className=' h-screen w-full flex flex-col items-center py-10 gap-2'>
+            <div className='border w-[80%] h-80 rounded-3xl flex flex-wrap justify-center items-center gap-10 py-5 px-10 '>
                 <div>
-                    <figure className='border rounded-full w-30 h-30'><img src="/github-icon-2.svg" alt="" /></figure>
+                    <figure className=' border rounded-full w-30 h-30'><img src="./github-icon-2.svg" alt="" /></figure>
                 </div>
                 <div className='text-xl flex flex-col gap-3'>
 
@@ -19,20 +19,20 @@ export default function DashboardComponent() {
             <div className='border w-[80%] h-50 rounded-3xl flex gap-10 text-[12px] items-center justify-around px-1.5' >
 
                 <div className=' border w-[30%]  h-[150px]  rounded-3xl flex flex-wrap justify-center items-center py-2 px-1 '>
-                    <p className='border'>Horas de servicio Requeridas</p>
-                    <p role='horas esperadas' className='border text-5xl'>
+                    <p className=''>Horas de servicio Requeridas</p>
+                    <p role='horas esperadas' className=' text-5xl'>
                         20
                     </p>
                 </div>
                 <div className=' border w-[30%]  h-[150px]  rounded-3xl flex flex-wrap justify-center items-center py-2 px-1 '>
                     <p>Horas de servicio Requeridas</p>
-                    <p role='horas registradas' className='border text-5xl'>
+                    <p role='horas registradas' className=' text-5xl'>
                         15
                     </p>
                 </div>
                 <div className=' border w-[30%]  h-[150px]  rounded-3xl flex flex-wrap justify-center items-center py-2 px-1 '>
                     <p>Horas de servicio autorizadas</p>
-                    <p role='horas autorizadas' className='border text-5xl'>
+                    <p role='horas autorizadas' className=' text-5xl'>
                         10
                     </p>
                 </div>
