255f246d8c52d139b9cdd9e0f1f08e2e81f96ce4
Julio0885 <julioc0885@gmail.com>
2025-05-06 11:59:04 -0500

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Aside from "./components/Aside";
 import Login from "./components/Login";
 import { Route, Routes } from "react-router";
+import HorasServicio from "./components/HorasServicio";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Aside />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/horas-servicio" element={<HorasServicio />} />
       </Routes>
     </div>
   );
