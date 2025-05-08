72eb9cd3820b5a5b9dd3006865663ee7b78302f6
adrianasheila <adrisheiaquiblan@gmail.com>
2025-05-08 15:05:50 -0400

    se corrigio el registro de horas de servicio

--- src/axios/auth/login.js
+++ src/axios/auth/login.js
@@ -133,9 +133,9 @@ export async function editarActividad(data, id) {
 
 export async function load(id) {
   try {
-    const { data } = await instance.get(`/services/${id}`, {
+    const { data } = await instance.get(`/evidence/${id}`, {
       headers: {
-        "Content-Type": "multipart/form-data",
+        "Content-Type": "application/pdf",
         Accept: "application/pdf",
       },
       responseType: "blob",
--- src/components/EditarActividadForm.jsx
+++ src/components/EditarActividadForm.jsx
@@ -1,41 +1,42 @@
 import React, { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { editarActividad } from "../axios/auth/login";
+import { useNavigate } from "react-router";
 
 export default function EditarActividadForm({ setShowModalEdit, item }) {
-  const { register, handleSubmit, reset } = useForm();
-  const itemId = item.id;
-  console.log(itemId);
+  const { register, reset } = useForm();
+  const navigate = useNavigate();
   useEffect(() => {
     if (item) {
       reset({
         amount_reported: item.amount_reported,
         description: item.description,
         category_id: item.category.id,
-        evidence: item.evidence,
       });
     }
   }, [item, reset]);
 
-  const handleEdit = async (data) => {
+  async function handleEdit(e) {
     try {
-      const status = await editarActividad(data, itemId);
-      console.log(status);
+      e.preventDefault();
+      const requestData = new FormData(e.target);
+      const status = await editarActividad(requestData, item.id);
       if (status === 200) {
         alert("Actividad editada con éxito");
         setShowModalEdit(false);
+        navigate(0);
       }
     } catch (error) {
       console.log("Error al editar la actividad:", error);
     }
-  };
+  }
 
   return (
     <div className="fixed inset-0 z-10 flex justify-center items-center rounded-lg">
       <div className="bg-indigo-50 p-6 rounded-lg w-80 md:w-96">
         <h2 className="text-lg font-bold mb-4">Editar Actividad</h2>
         <form
-          onSubmit={handleSubmit(handleEdit)}
+          onSubmit={handleEdit}
           encType="multipart/form-data"
           className="flex flex-col gap-3"
         >
@@ -43,7 +44,7 @@ export default function EditarActividadForm({ setShowModalEdit, item }) {
             <label className="text-sm">Horas reportadas</label>
             <input
               type="number"
-              {...register("amount_reported")}
+              {...register("amount_reported", { valueAsNumber: true })}
               required
               className="border w-full p-1 rounded"
             />
