70433b34c0b8e5aa1e47329cb22308921f713f73
adrianasheila <adrisheiaquiblan@gmail.com>
2025-05-08 18:15:11 -0400

    revision de horas de servicio

--- src/axios/users/users.js
+++ src/axios/users/users.js
@@ -1,87 +1,91 @@
 import { instance } from "../instance";
 
 export async function services() {
-    try {
-      const { data } = await instance.get(`/services`);
-      return data;
-    } catch (error) {
-      throw error;
-    }
+  try {
+    const { data } = await instance.get(`/services`);
+    return data;
+  } catch (error) {
+    throw error;
   }
-  
-  export async function users() {
-    try {
-      const { data } = await instance.get(`/users`);
-      return data;
-    } catch (error) {
-      throw error;
-    }
+}
+
+export async function users() {
+  try {
+    const { data } = await instance.get(`/users`);
+    return data;
+  } catch (error) {
+    throw error;
   }
+}
 
-  export async function deleteUsers(id) {
-    try {
-      const { status } = await instance.delete(`/users/${id}`);
-      return status;
-    } catch (error) {
-      throw error;
-    }
+export async function deleteUsers(id) {
+  try {
+    const { status } = await instance.delete(`/users/${id}`);
+    return status;
+  } catch (error) {
+    throw error;
   }
+}
 
-
-
-  
-  export async function servicesIdUser(id) {
-    try {
-      const { data } = await instance.get(`/services/?status=&&user=${id}`);
-      return data;
-    } catch (error) {
-      throw error;
-    }
+export async function servicesIdUser(id) {
+  try {
+    const { data } = await instance.get(`/services/?status=&&user=${id}`);
+    return data;
+  } catch (error) {
+    throw error;
   }
-  
-  export async function createUser(request) {
-    try {
-      const { status } = await instance.post(`/users`, request);
-      return status;
-    } catch (error) {
-      throw error;
-    }
-  }
+}
 
-  export async function getRoles() {
-    try {
-      const { data } = await instance.get(`/roles`);
-      return data;
-    } catch (error) {
-      throw error;
-    }
+export async function createUser(request) {
+  try {
+    const { status } = await instance.post(`/users`, request);
+    return status;
+  } catch (error) {
+    throw error;
   }
+}
 
-
-  export async function getUserByRol(id) {
-    try {
-      const { data } = await instance.get(`/users?r=${id}`);
-      return data;
-    } catch (error) {
-      throw error;
-    }
+export async function getRoles() {
+  try {
+    const { data } = await instance.get(`/roles`);
+    return data;
+  } catch (error) {
+    throw error;
   }
+}
 
-  export async function getSchoolsList() {
-    try {
-      const { data } = await instance.get(`/schools`);
-      return data;
-    } catch (error) {
-      throw error;
-    }
+export async function getUserByRol(id) {
+  try {
+    const { data } = await instance.get(`/users?r=${id}`);
+    return data;
+  } catch (error) {
+    throw error;
   }
+}
 
-  export async function servicesId(id) {
-    try {
-      const { data } = await instance.get(`/services/${id}`);
-      return data;
-    } catch (error) {
-      throw error;
-    }
+export async function getSchoolsList() {
+  try {
+    const { data } = await instance.get(`/schools`);
+    return data;
+  } catch (error) {
+    throw error;
   }
-  
+}
+
+export async function servicesId(id) {
+  try {
+    const { data } = await instance.get(`/services/${id}`);
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function revisionHorasServicio(request, id) {
+  try {
+    const { data } = await instance.patch(`/services/${id}/review`, request);
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
